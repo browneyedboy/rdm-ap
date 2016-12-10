a93a84ae2c21249357baa7d21418ec593971e076
Amarsanaa <amarsanaa@outlook.com>
2016-12-10 18:52:48 +0800

    profile sml edt

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import * as Parse from 'parse';
 import { EditprofilePage } from '../editprofile/editprofile';
 /*
   Generated class for the Profile page.
@@ -13,11 +13,33 @@ import { EditprofilePage } from '../editprofile/editprofile';
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
-
+	public firstname;
+	public email;
+	public phone;
   constructor(public navCtrl: NavController) {}
 
   ionViewDidLoad() {
+  	var currentUser = Parse.User.current();
+  	console.log(currentUser.id);
     console.log('Hello ProfilePage Page');
+
+	// var query = new Parse.Query(Parse.User);
+	// query.equalTo("objectId", currentUser.id);  // find all the women
+	// query.find({
+	//   success: function(user) {
+	//     	console.log(user.username);
+	//   }
+	// });
+	var this_ref = this;
+	Parse.User.current().fetch().then(function (user) {
+	    console.log(user.get('email') );
+	    console.log(user.get('firstname') );
+	    console.log(user.get('lastname') );
+	    this_ref.firstname = user.get('firstname');
+	    this_ref.email = user.get('email');
+	    this_ref.phone = user.get('phone');
+	});
+
   }
 
   edit(){
